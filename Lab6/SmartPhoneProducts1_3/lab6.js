70e34b85321058e4639fde924065ecee861010d4
ethankschwarz <ethanks@hawaii.edu>
2023-09-26 17:05:51 -1000

    changes

--- Lab6/SmartPhoneProducts1_3/lab6.js
+++ Lab6/SmartPhoneProducts1_3/lab6.js
@@ -1,40 +1,40 @@
-function calculateDays() {
-    // Get the input value from the user
-    const monthInput = document.getElementById('monthInput').value;
+//This is an order that is being called that will be sent to the DOM. Both of these will make sure that the month input and the result will assign a value for the element. 
+let monthInput=document.getElementById("monthInput");
+let result=document.getElementById("result");
+//Only when button is selected will the function run
+document.querySelector('button').addEventListener('click', function () {
+    let month=monthInput.value.toLowerCase();
 
-    // Initialize the variable to store the number of days
-    let numDays;
-
-    // Use a switch statement to determine the number of days based on the month
-    switch (monthInput.toLowerCase()) {
-        case 'january':
-        case 'march':
-        case 'may':
-        case 'july':
-        case 'august':
-        case 'october':
-        case 'december':
-            numDays = 31;
+//default value also can be an error on the input 
+    let num_days =-1;
+    //switch is introduced to change the output dependent on what the input is. This can also match expressions. Case will execute the block of code. 
+    switch(month) {
+        case "january":
+        case "march":
+        case "may":
+        case "july":
+        case "august":
+        case "october":
+        case "december":
+            num_days=31;
             break;
-        case 'april':
-        case 'june':
-        case 'september':
-        case 'november':
-            numDays = 30;
+        case "april":
+        case "june":
+        case "september":
+        case "november":
+            num_days=30;
             break;
-        case 'february':
-            numDays = 28; // Assuming non-leap year by default
+        case "february":
+            num_days=28;
             break;
         default:
-            numDays = -1; // Indicates an error
+            num_days=-1;
             break;
     }
-
-    // Display the result in the HTML page
-    const resultElement = document.getElementById('result');
-    if (numDays === -1) {
-        resultElement.textContent = 'Invalid month entered. Please enter a valid month name.';
+//if it is not spelled correctly it will say invalid, but if not the switch will comense and you will have the days of the month.
+    if (num_days === -1){
+        result.textContent ='Invalid Month. Please enter a valid month name';
     } else {
-        resultElement.textContent = `There are ${numDays} days in ${monthInput}.`;
+        result.textContent = `There are ${num_days} days in ${month}.`;
     }
-}
+    });
